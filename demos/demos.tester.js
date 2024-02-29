0482e8cbef342f1f94564b2a0e99768f7a8075ec
Ganesh B <ganeshsurfs@gmail.com>
2024-02-29 07:32:31 +0530

    [COMMIT] Adding minor changes to package. Adding file for index.js. Adding package.json deps for testing

---
+++ demos/demos.tester.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i which-arraytype --save
+ * Github: https://github.com/ganeshkbhat/which-arraytype/
+ * npmjs Link: https://www.npmjs.com/package/which-arraytype/
+ * File: index.js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
