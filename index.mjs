68d017d9ded06662c901cdc063e968bcf4df709d
Ganesh B <ganeshsurfs@gmail.com>
2024-02-29 17:28:36 +0530

    [COMMIT] Adding minor changes in doc comments. Adding index.mjs file

--- index.mjs
+++ index.mjs
@@ -1,5 +1,20 @@
+/**
+ * 
+ * Package: which-istypedarray
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i which-istypedarray --save
+ * Github: https://github.com/ganeshkbhat/which-istypedarray/
+ * npmjs Link: https://www.npmjs.com/package/which-istypedarray/
+ * File: index.js
+ * File Description: 
+ * 
+ * 
+*/
 
+/* eslint no-console: 0 */
 
+'use strict';
 
 import * as istypedarray from "./index";
 
